104a5b8fc2c201834d0c375b8d13f8f5f329102f
KMJ <minj20@duksung.ac.kr>
2025-04-27 22:59:05 +0900

    ✨ Feat: User 엔티티 로그아웃 관련 필드 추가 #6

---
+++ src/main/java/com/solit/sync2sing/domain/user/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.solit.sync2sing.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@Entity
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String gender;
+    private int age;
+    private String voiceRange;
+    private String voiceType;
+    private int duetPenaltyCount;
+    private LocalDateTime duetPenaltyUntil;
+
+    private String refreshToken;
+
+}
