b9d91309581a2ae8b92bc0812e2b39d387945306
KMJ <minj20@duksung.ac.kr>
2025-04-29 19:45:51 +0900

    ✨ Feat: 로그아웃 시 AccessToken 블랙리스트 등록 기능 추가 #6

--- src/main/java/com/solit/sync2sing/domain/user/LogoutServiceImplementation.java
+++ src/main/java/com/solit/sync2sing/domain/user/LogoutServiceImplementation.java
@@ -7,8 +7,11 @@ import com.solit.sync2sing.domain.user.repository.UserRepository;
 import com.solit.sync2sing.domain.user.service.UserLogoutService;
 import com.solit.sync2sing.global.response.ResponseCode;
 import com.solit.sync2sing.global.security.TokenProvider;
+import org.springframework.data.redis.core.RedisTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -16,18 +19,39 @@ public class LogoutServiceImplementation implements UserLogoutService {
 
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
-    public LogoutResponseDTO logout(String accessToken, LogoutRequestDTO logoutRequestDTO) {
+    public LogoutResponseDTO logout(String accessToken, LogoutRequestDTO
+            logoutRequestDTO) {
+
+        // accessToken 만료 검증
+        if (tokenProvider.isTokenExpired(accessToken)) {
+            throw new RuntimeException(ResponseCode.EXPIRED_JWT_TOKEN.getMessage());
+        }
 
         String userEmail = tokenProvider.getUsernameFromToken(accessToken);
 
         UserEntity user = userRepository.findByUsername(userEmail)
                 .orElseThrow(() -> new RuntimeException(ResponseCode.USER_NOT_FOUND.getMessage()));
 
-        if (!user.getRefreshToken().equals(logoutRequestDTO.getRefreshToken())) {
+        // refreshToken 유효성 검사
+        String refreshToken = logoutRequestDTO.getRefreshToken();
+        if (user.getRefreshToken() == null || !user.getRefreshToken().equals(logoutRequestDTO.getRefreshToken())) {
             throw new RuntimeException(ResponseCode.INVALID_JWT_TOKEN.getMessage());
         }
+
+        // 4. AccessToken 블랙리스트 등록
+        Long expiration = tokenProvider.getExpirationFromToken(accessToken);
+        if (expiration <= 0) {
+            throw new RuntimeException(ResponseCode.EXPIRED_JWT_TOKEN.getMessage());
+        }
+        redisTemplate.opsForValue().set(
+                accessToken,
+                "blacklisted",
+                expiration,
+                TimeUnit.MILLISECONDS
+        );
 
         // refreshToken 삭제 (DB에 null로 업데이트)
         user.setRefreshToken(null);
