fbefe9f7baa56c06ee11f302a50995b0687517b4
KMJ <minj20@duksung.ac.kr>
2025-04-29 19:53:26 +0900

    ✨  Feat: 로그인 서비스 인터페이스 작성 #5

--- src/main/java/com/solit/sync2sing/domain/user/service/UserLoginService.java
+++ src/main/java/com/solit/sync2sing/domain/user/service/UserLoginService.java
@@ -1,4 +1,8 @@
 package com.solit.sync2sing.domain.user.service;
 
+import com.solit.sync2sing.domain.user.dto.request.LoginRequestDTO;
+import com.solit.sync2sing.domain.user.dto.response.LoginResponseDTO;
+
 public interface UserLoginService {
+    LoginResponseDTO login(LoginRequestDTO requestDTO);
 }
