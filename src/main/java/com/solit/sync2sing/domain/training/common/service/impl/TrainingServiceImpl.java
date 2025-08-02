f995fb65d3544148ba7f49eaa552fdbccb1a5464
Narae Gwon <rnjs021031@gmail.com>
2025-08-03 01:27:09 +0900

    ✨ Feat: 솔로 보컬 분석 리포트 생성 기능 구현 (PRE, POST) #38

--- src/main/java/com/solit/sync2sing/domain/training/common/service/impl/TrainingServiceImpl.java
+++ src/main/java/com/solit/sync2sing/domain/training/common/service/impl/TrainingServiceImpl.java
@@ -6,6 +6,7 @@ import com.solit.sync2sing.domain.training.common.service.TrainingService;
 import com.solit.sync2sing.entity.*;
 import com.solit.sync2sing.global.ai.dto.AiVoiceAnalysisResponse;
 import com.solit.sync2sing.global.ai.service.AiService;
+import com.solit.sync2sing.global.chatgpt.dto.SoloPostResponse;
 import com.solit.sync2sing.global.chatgpt.dto.SoloPreResponse;
 import com.solit.sync2sing.global.chatgpt.sevice.ChatGPTService;
 import com.solit.sync2sing.global.response.ResponseCode;
@@ -44,6 +45,7 @@ class TrainingServiceImpl implements TrainingService {
     private final TrainingSessionTrainingRepository trainingSessionTrainingRepository;
     private final UserTrainingLogRepository userTrainingLogRepository;
     private final SongRepository songRepository;
+    private final LyricslineRepository lyricslineRepository;
     private final VocalAnalysisReportRepository vocalAnalysisReportRepository;
 
     @Override
@@ -391,7 +393,7 @@ class TrainingServiceImpl implements TrainingService {
 
             VocalAnalysisReport vocalAnalysisReport = VocalAnalysisReport.builder()
                     .song(guestSong)
-                    .title(vocalAnalysisReportTitle("Do-Re-Mi"))
+                    .title(vocalAnalysisReportTitle(guestSong.getTitle()))
                     .trainingMode(TrainingMode.SOLO)
                     .reportType(RecordingContext.GUEST)
                     .pitchScore(request.getPitchAccuracy())
@@ -422,7 +424,117 @@ class TrainingServiceImpl implements TrainingService {
             GenerateVocalAnalysisReportRequest request,
             CustomUserDetails userDetails
     ) {
-        return null;
+        // 사용자 세션 곡 조회
+        TrainingSession session = trainingSessionRepository.findByUser(userDetails.getUser()).stream()
+                .filter(s -> s.getTrainingMode().name().equals(request.getTrainingMode()))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        ResponseCode.TRAINING_SESSION_NOT_FOUND.getStatus(),
+                        ResponseCode.TRAINING_SESSION_NOT_FOUND.getMessage()
+                ));
+        Song trsiningSong = session.getSong();
+
+        String recordingAudioS3Url = null;
+
+        try {
+            recordingAudioS3Url = s3Util.saveRecordingAudioToS3(vocalFile);
+            String jobName = "transcripts_job-" + UUID.randomUUID();
+
+            CompletableFuture<String> transcriptFuture = transcriptionService.transcribeAndGetText(jobName, recordingAudioS3Url);
+            CompletableFuture<AiVoiceAnalysisResponse> aiFuture = aiService.analyzeWithAiServer(recordingAudioS3Url);
+
+            String transcriptText = transcriptFuture.get(60, TimeUnit.SECONDS);
+            AiVoiceAnalysisResponse aiResult = aiFuture.get(60, TimeUnit.SECONDS);
+
+            List<String> typeList = new ArrayList<>();
+            List<String> ratioList = new ArrayList<>();
+
+            for (int i = 0; i < aiResult.getData().getTop_voice_types().size(); i++) {
+                String type = aiResult.getData().getTop_voice_types().get(i).getType();
+                Double ratio = aiResult.getData().getTop_voice_types().get(i).getRatio();
+
+                typeList.add(type);
+                ratioList.add(String.format("%.3f", ratio));
+            }
+
+            // 전체 가사 조회
+            List<Lyricsline> lines = lyricslineRepository.findBySongOrderByLineIndex(trsiningSong);
+            String lyricText = "";
+            lyricText += lines.stream()
+                    .map(Lyricsline::getText) + " ";
+
+            int pronunciationScore = calculateSimilarityScore(transcriptText, lyricText);
+
+            // 호흡 평가
+            int breathScore = 60;
+
+            String userPrompt = "사용자의 음정, 박자, 발음, 호흡 점수와 사용자의 발성 유형 예측 결과 가장 확률이 높은 상위 3개 태그와 그 확률을 알려줄게.\n" +
+                    "너는 총평 제목(overallReviewTitle), 총평 내용(overallReviewContent), 총평의 원인(causeContent), 추가적인 제안(proposalContent) 4가지를 알려줘.\n" +
+                    "\n" +
+                    "아래 네 가지 항목을 JSON 형식의 문자열로 답변해줘. 다른 말은 하지 말고 오직 JSON 형식의 문자열 응답만 줘.\n" +
+                    "{\n" +
+                    "  \"overallReviewTitle\": \"\",\n" +
+                    "  \"overallReviewContent\": \"\",\n" +
+                    "  \"causeContent\": \"\",\n" +
+                    "  \"proposalContent\": \"\"\n" +
+                    "}\n" +
+                    "음정 점수: " + request.getPitchAccuracy() + "\n" +
+                    "박자 점수: " + request.getBeatAccuracy() + "\n" +
+                    "발음 점수: " + pronunciationScore + "\n" +
+                    "호흡 점수: " + breathScore + "\n" +
+                    "발성 태그와 예측 확률: " + "\n" +
+                    typeList.get(0) + " " + ratioList.get(0) + "\n" +
+                    typeList.get(1) + " " + ratioList.get(1) + "\n" +
+                    typeList.get(2) + " " + ratioList.get(2) + "\n" +
+                    "다음은 너의 답변 예시를 알려줄게.\n" +
+                    "\n" +
+                    "{\n" +
+                    "  \"overallReviewTitle\": \"호흡이 큰 장점이지만, 음정과 박자에 안정이 필요해요\",\n" +
+                    "  \"overallReviewContent\": \"호흡 조절은 잘하고 계시지만, 음정과 박자가 불안정하여 노래의 화성 구조를 충분히 표현하지 못하고 있어요. 발성은 중간 정도로 괜찮지만, 정확한 음정과 리듬을 통해 전체적인 완성도를 높일 필요가 있습니다.\",\n" +
+                    "  \"causeContent\": \"코드 변화를 정확히 인지하지 못해 화성 진행에 따른 음의 변화를 자연스럽게 표현하기 어려워요.\",\n" +
+                    "  \"proposalContent\": \"주요 코드(C, F, G)의 느낌을 익히고, 단순한 발성 연습부터 시작해 듣기 훈련을 병행하세요.\"\n" +
+                    "}";
+
+            String gptResponse = chatGPTService.askToGpt(userPrompt);
+
+            gptResponse = gptResponse.replaceAll("```json|```", "").trim();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            SoloPreResponse soloPreResponse = objectMapper.readValue(gptResponse, SoloPreResponse.class);
+
+            String overallReviewTitle = soloPreResponse.getOverallReviewTitle();
+            String overallReviewContent = soloPreResponse.getOverallReviewContent();
+            String causeContent = soloPreResponse.getCauseContent();
+            String proposalContent = soloPreResponse.getProposalContent();
+
+            s3Util.deleteFileFromS3(recordingAudioS3Url);
+
+            VocalAnalysisReport vocalAnalysisReport = VocalAnalysisReport.builder()
+                    .song(trsiningSong)
+                    .title(vocalAnalysisReportTitle(trsiningSong.getTitle()))
+                    .trainingMode(TrainingMode.SOLO)
+                    .reportType(RecordingContext.PRE)
+                    .pitchScore(request.getPitchAccuracy())
+                    .beatScore(request.getBeatAccuracy())
+                    .pronunciationScore(pronunciationScore)
+                    .breathScore(breathScore)
+                    .overallReviewTitle(overallReviewTitle)
+                    .overallReviewContent(overallReviewContent)
+                    .causeContent(causeContent)
+                    .proposalContent(proposalContent)
+                    .build();
+
+            vocalAnalysisReportRepository.save(vocalAnalysisReport);
+
+            return PreVocalAnalysisReportResponse.toDTO(vocalAnalysisReport);
+        } catch (Exception e) {
+            if (recordingAudioS3Url != null) s3Util.deleteFileFromS3(recordingAudioS3Url);
+
+            throw new ResponseStatusException(
+                    ResponseCode.FILE_UPLOAD_FAIL_S3_ROLLBACK.getStatus(),
+                    ResponseCode.FILE_UPLOAD_FAIL_S3_ROLLBACK.getMessage()
+            );
+        }
     }
 
     private GenerateVocalAnalysisReportResponse soloPostAnalysis(
@@ -430,7 +542,129 @@ class TrainingServiceImpl implements TrainingService {
             GenerateVocalAnalysisReportRequest request,
             CustomUserDetails userDetails
     ) {
-        return null;
+        // 사용자 세션 곡 조회
+        TrainingSession session = trainingSessionRepository.findByUser(userDetails.getUser()).stream()
+                .filter(s -> s.getTrainingMode().name().equals(request.getTrainingMode()))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        ResponseCode.TRAINING_SESSION_NOT_FOUND.getStatus(),
+                        ResponseCode.TRAINING_SESSION_NOT_FOUND.getMessage()
+                ));
+        Song trsiningSong = session.getSong();
+
+        // 훈련 전 보컬분석리포트
+        VocalAnalysisReport preReport = vocalAnalysisReportRepository
+                .findTopBySongAndTrainingModeAndReportTypeOrderByCreatedAtDesc(
+                        trsiningSong, TrainingMode.SOLO, RecordingContext.PRE
+                )
+                .orElse(null);
+
+        String recordingAudioS3Url = null;
+
+        try {
+            recordingAudioS3Url = s3Util.saveRecordingAudioToS3(vocalFile);
+            String jobName = "transcripts_job-" + UUID.randomUUID();
+
+            CompletableFuture<String> transcriptFuture = transcriptionService.transcribeAndGetText(jobName, recordingAudioS3Url);
+            CompletableFuture<AiVoiceAnalysisResponse> aiFuture = aiService.analyzeWithAiServer(recordingAudioS3Url);
+
+            String transcriptText = transcriptFuture.get(60, TimeUnit.SECONDS);
+            AiVoiceAnalysisResponse aiResult = aiFuture.get(60, TimeUnit.SECONDS);
+
+            List<String> typeList = new ArrayList<>();
+            List<String> ratioList = new ArrayList<>();
+
+            for (int i = 0; i < aiResult.getData().getTop_voice_types().size(); i++) {
+                String type = aiResult.getData().getTop_voice_types().get(i).getType();
+                Double ratio = aiResult.getData().getTop_voice_types().get(i).getRatio();
+
+                typeList.add(type);
+                ratioList.add(String.format("%.3f", ratio));
+            }
+
+            // 전체 가사 조회
+            List<Lyricsline> lines = lyricslineRepository.findBySongOrderByLineIndex(trsiningSong);
+            String lyricText = "";
+            lyricText += lines.stream()
+                    .map(Lyricsline::getText) + " ";
+
+            int pronunciationScore = calculateSimilarityScore(transcriptText, lyricText);
+
+            // 호흡 평가
+            int breathScore = 60;
+
+            String userPrompt = "사용자의 보컬 훈련 전과 훈련 후의 각 음정, 박자, 발음, 호흡 점수와 훈련 후 사용자의 발성 유형 예측 결과 가장 확률이 높은 상위 3개 태그와 그 확률을 알려줄게.\n" +
+                    "너는 훈련 후 보컬에 대한 총평 제목(overallReviewTitle), 총평 내용(overallReviewContent), 훈련 전후 차이에 대한 피드백 제목(feedbackTitle), 피드백 내용(feedbackContent) 4가지를 알려줘.\n" +
+                    "\n" +
+                    "아래 네 가지 항목을 JSON 형식의 문자열로 답변해줘. 다른 말은 하지 말고 오직 JSON 형식의 문자열 응답만 줘.\n" +
+                    "{\n" +
+                    "  \"overallReviewTitle\": \"\",\n" +
+                    "  \"overallReviewContent\": \"\",\n" +
+                    "  \"feedbackTitle\": \"\",\n" +
+                    "  \"feedbackContent\": \"\"\n" +
+                    "}\n" +
+                    "훈련 전 음정 점수: " + preReport.getPitchScore() + "\n" +
+                    "훈련 전 박자 점수: " + preReport.getBeatScore() + "\n" +
+                    "훈련 전 발음 점수: " + preReport.getPronunciationScore() + "\n" +
+                    "훈련 전 호흡 점수: " + preReport.getBreathScore() + "\n" +
+                    "훈련 후 음정 점수: " + request.getPitchAccuracy() + "\n" +
+                    "훈련 후 박자 점수: " + request.getBeatAccuracy() + "\n" +
+                    "훈련 후 발음 점수: " + pronunciationScore + "\n" +
+                    "훈련 후 호흡 점수: " + breathScore + "\n" +
+                    "훈련 후 발성 태그와 예측 확률: " + "\n" +
+                    typeList.get(0) + " " + ratioList.get(0) + "\n" +
+                    typeList.get(1) + " " + ratioList.get(1) + "\n" +
+                    typeList.get(2) + " " + ratioList.get(2) + "\n" +
+                    "다음은 너의 답변 예시를 알려줄게.\n" +
+                    "\n" +
+                    "{\n" +
+                    "  \"overallReviewTitle\": \"호흡이 큰 장점이지만, 음정과 박자에 안정이 필요해요\",\n" +
+                    "  \"overallReviewContent\": \"호흡 조절은 잘하고 계시지만, 음정과 박자가 불안정하여 노래의 화성 구조를 충분히 표현하지 못하고 있어요. 발성은 중간 정도로 괜찮지만, 정확한 음정과 리듬을 통해 전체적인 완성도를 높일 필요가 있습니다.\",\n" +
+                    "  \"feedbackTitle\": \"꾸준함의 힘, 눈에 띄는 성장\",\n" +
+                    "  \"feedbackContent\": \"전반적인 퍼포먼스가 크게 향상되었습니다. 훈련 진행률 85%를 달성하였으며, 꾸준한 연습을 통해 더욱 발전할 수 있습니다.\"\n" +
+                    "}";
+
+            String gptResponse = chatGPTService.askToGpt(userPrompt);
+
+            gptResponse = gptResponse.replaceAll("```json|```", "").trim();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            SoloPostResponse soloPostResponse = objectMapper.readValue(gptResponse, SoloPostResponse.class);
+
+            String overallReviewTitle = soloPostResponse.getOverallReviewTitle();
+            String overallReviewContent = soloPostResponse.getOverallReviewContent();
+            String feedbackTitle = soloPostResponse.getFeedbackTitle();
+            String feedbackContent = soloPostResponse.getFeedbackContent();
+
+            s3Util.deleteFileFromS3(recordingAudioS3Url);
+
+            VocalAnalysisReport vocalAnalysisReport = VocalAnalysisReport.builder()
+                    .song(trsiningSong)
+                    .title(vocalAnalysisReportTitle(trsiningSong.getTitle()))
+                    .trainingMode(TrainingMode.SOLO)
+                    .reportType(RecordingContext.POST)
+                    .pitchScore(request.getPitchAccuracy())
+                    .beatScore(request.getBeatAccuracy())
+                    .pronunciationScore(pronunciationScore)
+                    .breathScore(breathScore)
+                    .overallReviewTitle(overallReviewTitle)
+                    .overallReviewContent(overallReviewContent)
+                    .feedbackTitle(feedbackTitle)
+                    .feedbackContent(feedbackContent)
+                    .preTrainingReport(preReport)
+                    .build();
+
+            vocalAnalysisReportRepository.save(vocalAnalysisReport);
+
+            return PostVocalAnalysisReportResponse.toDTO(vocalAnalysisReport);
+        } catch (Exception e) {
+            if (recordingAudioS3Url != null) s3Util.deleteFileFromS3(recordingAudioS3Url);
+
+            throw new ResponseStatusException(
+                    ResponseCode.FILE_UPLOAD_FAIL_S3_ROLLBACK.getStatus(),
+                    ResponseCode.FILE_UPLOAD_FAIL_S3_ROLLBACK.getMessage()
+            );
+        }
     }
 
     private GenerateVocalAnalysisReportResponse duetPostAnalysis(
